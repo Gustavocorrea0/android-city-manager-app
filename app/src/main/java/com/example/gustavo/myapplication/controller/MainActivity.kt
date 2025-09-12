0090d5ab80598f7fd855c7fb43158aad7d858cce
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2025-09-11 21:44:42 -0300

    add remove city function

--- app/src/main/java/com/example/gustavo/myapplication/controller/MainActivity.kt
+++ app/src/main/java/com/example/gustavo/myapplication/controller/MainActivity.kt
@@ -1,25 +1,119 @@
 package com.example.gustavo.myapplication.controller
 
 import android.content.Intent
+import android.gesture.Gesture
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gustavo.myapplication.R
 import com.example.gustavo.myapplication.databinding.ActivityMainBinding
 import com.example.gustavo.myapplication.model.DataStore
 import com.example.gustavo.myapplication.view.CityAdapter
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var adapter: CityAdapter
     private lateinit var binding: ActivityMainBinding
+    private lateinit var gesture: GestureDetector
+
+    /*private val addCityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult) {
+        result -> if (result.resultCode == RESULT_OK) {
+            adapter.notifyDataSetChanged()
+            result.date?.let {
+                data -> data.getStringExtra("cityName")?.let {
+                    name ->
+                Snackbar.make(binding.main, "Cidade $name criada com sucesso!!!", Snackbar.LENGTH_LONG).show()
+            }
+            }
+        }
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        this.configureUI()
+        this.loadRecyclerViewData()
+        this.configureFab()
+        this.configureGestures()
+        this.configureRecycleViewWithEvents()
+    }
+
+    fun configureFab() {
+        binding.fab.setOnClickListener {
+            Intent(this, ManagerActivity::class.java).also { i ->
+                startActivity(i)
+            }
+        }
+    }
+
+    fun configureGestures(){
+        gesture = GestureDetector(this, object:GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+                binding.rcvCities.findChildViewUnder(e.x, e.y).also { view ->
+                    view?.let {
+                        child -> binding.rcvCities.getChildAdapterPosition(child).also { position ->
+                            val city = DataStore.getCity(position)
+                        Toast.makeText(this@MainActivity, city.name, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+                return super.onSingleTapConfirmed(e)
+            }
+
+            override fun onLongPress(e: MotionEvent) {
+                binding.rcvCities.findChildViewUnder(e.x, e.y).also { view ->
+                    view?.let {
+                            child -> binding.rcvCities.getChildAdapterPosition(child).also { position ->
+                            val city = DataStore.getCity(position)
+                            AlertDialog.Builder(this@MainActivity).also {  dialog ->
+                                dialog.setTitle("Cities App")
+                                dialog.setMessage("Tem certeza que deseja excluir a cidade: ${city.name}")
+                                dialog.setPositiveButton(android.R.string.ok) { _, _ ->
+                                    DataStore.delCity(position)
+                                    this@MainActivity.showMessage("Cidade ${city.name} removida com sucesso!!!")
+                                    adapter.notifyDataSetChanged()
+                                }
+                                dialog.setNegativeButton(android.R.string.cancel, null)
+                                dialog.show()
+                            }
+                        }
+                    }
+                }
+                super.onLongPress(e)
+            }
+        })
+    }
+
+    fun configureRecycleViewWithEvents(){
+        binding.rcvCities.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener{
+            override fun onInterceptTouchEvent( rv: RecyclerView, e: MotionEvent): Boolean {
+                rv.findChildViewUnder(e.x, e.y).also { view ->
+                    return (view != null && gesture.onTouchEvent(e))
+                }
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    fun configureUI(){
         enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -27,19 +121,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
+    fun loadRecyclerViewData() {
         LinearLayoutManager(this).also { llm ->
             llm.orientation = LinearLayoutManager.VERTICAL
             binding.rcvCities.layoutManager = llm
             adapter = CityAdapter(DataStore.cities)
             binding.rcvCities.adapter = adapter // populando a recicle view com dados
         }
-
-        binding.fab.setOnClickListener {
-            Intent(this, ManagerActivity::class.java).also { i ->
-                startActivity(i)
-            }
-        }
-
     }
+
+    fun showMessage(message: String) {
+        Snackbar.make(binding.main, message, Snackbar.LENGTH_LONG).show()
+    }
+
 }
--- app/src/main/java/com/example/gustavo/myapplication/controller/ManagerActivity.kt
+++ app/src/main/java/com/example/gustavo/myapplication/controller/ManagerActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gustavo.myapplication.controller
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gustavo.myapplication.R
 import com.example.gustavo.myapplication.databinding.ActivityManagerBinding
+import com.example.gustavo.myapplication.model.City
+import com.example.gustavo.myapplication.model.DataStore
+import com.google.android.material.snackbar.Snackbar
 
 class ManagerActivity : AppCompatActivity() {
 
@@ -14,24 +18,39 @@ class ManagerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityManagerBinding.inflate(layoutInflater)
+
+        this.configureUI()
+
+
+
+        // funcoes dos botoes
+        binding.btnSave.setOnClickListener {
+            val name = binding.txtName.text.toString()
+            val people = binding.txtPeople.text.toString().toIntOrNull() ?: 0 // se for nulo retorna 0
+            City(name, people).also { city ->
+                DataStore.addCity(city)
+            }
+            Intent().apply {
+                this.putExtra("cityName", name)
+                setResult(RESULT_OK, this)
+            }
+            finish()
+        }
+
+        binding.btnCancel.setOnClickListener {
+            setResult(RESULT_CANCELED)
+            finish()
+        }
+    }
+
+    fun configureUI() {
+        enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layManager)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // 2:33:31 - continuar
-        // funcoes dos botoes
-        binding.btnSave.setOnClickListener {
-
-        }
-
-        binding.btnCancel.setOnClickListener {
-            finish()
-        }
-
     }
 }
